255d177eacbc68789ce750c8d18defac25adfb5b
Olly McBride OB <ollymcbride@openbionics.com>
2021-06-24 16:39:36 +0100

    Converter.html creates elements rather than js

--- app.js
+++ app.js
@@ -1,103 +1,131 @@
-var auto_number_detection = true;
-var manual_entry = ""
 
 
-// on data entered into the 'Input' box
-document.getElementById("input").addEventListener("input", function(e) {
-    parse_input(e.target.value);
-})
+// var num_types = ["Dec", "Hex", "Bin", "ASCII"];
 
-function parse_input(num_str) {
-    console.log("Input number: " + num_str + " (auto?: " + auto_number_detection + ")");
+// function create_elements(num_type) {
+//     var label = document.createElement("LABEL");
+//     label.innerHTML = num_type;
+//     document.body.appendChild(label);
 
-    if (num_str != "") {             
-        var number = new NumberRep();
-        
-        if (auto_number_detection == true)
-        {
-            number.parse(num_str);
-        }
-        else
-        {
-            number.parse_manual(num_str, manual_entry);
-        }
-        
-        label_val = "Auto-detected: " + number.getType();
-        hex_val = number.toHex();
-        dec_val = number.toDec();
-        bin_val = number.toBin();
-        ascii_val = number.toASCII();
-        set_radio_btn(number.getType())
-    }
-    else {
-        label_val = "Enter number";
-        hex_val = "Hex";
-        dec_val = "Dec";
-        bin_val = "Bin";
-        ascii_val = "";
-    }
+//     var text_input = document.createElement("INPUT");
+//     text_input.setAttribute("type", "text");
+//     text_input.placeholder = num_type;
+//     document.body.appendChild(text_input);
 
-    document.getElementById("input_label").innerHTML = label_val;
-    document.getElementById("hex_box").placeholder = hex_val;
-    document.getElementById("dec_box").placeholder = dec_val;
-    document.getElementById("bin_box").placeholder = bin_val;
-    document.getElementById("ascii_box").placeholder = ascii_val;
-}
+//     var btn = document.createElement("BUTTON");
+//     btn.innerHTML = "*";
+//     document.body.appendChild(btn);
 
-// on auto/manual checkbox toggle
-document.getElementById("auto_manual").addEventListener("change", function(e) {
-    if (document.getElementById("auto_manual").checked) {
-        console.log("checked");
-        document.getElementById("manual_buttons").disabled = true;
-        auto_number_detection = true;
-        parse_input(document.getElementById("input").value);
-    }
-    else {
-        document.getElementById("manual_buttons").disabled = false;
-        console.log("un checked");
-        auto_number_detection = false;
-    }
-})
+//     var br = document.createElement("br");
+//     document.body.appendChild(br);
+// }
 
-// document.getElementsByName("type_select").addEventListener("input", function(e){
-//     console.log("Radio btn");
-//     console.log(e);
+// for (i of num_types)
+// {
+//     create_elements(i);
+// }
+
+
+// var auto_number_detection = true;
+// var manual_entry = ""
+
+
+// // on data entered into the 'Input' box
+// document.getElementById("input").addEventListener("input", function(e) {
+//     parse_input(e.target.value);
 // })
 
-if (document.querySelector('input[name="type_select"]')) {
-    document.querySelectorAll('input[name="type_select"]').forEach((elem) => {
-        elem.addEventListener("change", function(event) {
-        manual_entry = event.target.value;
-        console.log(manual_entry);
+// function parse_input(num_str) {
+//     console.log("Input number: " + num_str + " (auto?: " + auto_number_detection + ")");
 
-        parse_input(document.getElementById("input").value);
-      });
-    });
-  }
+//     if (num_str != "") {             
+//         var number = new NumberRep();
+        
+//         if (auto_number_detection == true)
+//         {
+//             number.parse(num_str);
+//         }
+//         else
+//         {
+//             number.parse_manual(num_str, manual_entry);
+//         }
+        
+//         label_val = "Auto-detected: " + number.getType();
+//         hex_val = number.toHex();
+//         dec_val = number.toDec();
+//         bin_val = number.toBin();
+//         ascii_val = number.toASCII();
+//         set_radio_btn(number.getType())
+//     }
+//     else {
+//         label_val = "Enter number";
+//         hex_val = "Hex";
+//         dec_val = "Dec";
+//         bin_val = "Bin";
+//         ascii_val = "";
+//     }
 
-// set one of the 'manual number representation' radio buttons to checked
-function set_radio_btn(number_format) {
+//     document.getElementById("input_label").innerHTML = label_val;
+//     document.getElementById("hex_box").placeholder = hex_val;
+//     document.getElementById("dec_box").placeholder = dec_val;
+//     document.getElementById("bin_box").placeholder = bin_val;
+//     document.getElementById("ascii_box").placeholder = ascii_val;
+// }
 
-    switch(number_format) {
-        case "Decimal":
-            btn_id = "select_dec";
-            break;
-        case "Hex":
-            btn_id = "select_hex";
-            break;
-        case "Binary":
-            btn_id = "select_bin";
-            break;
-        case "ASCII":
-            btn_id = "select_ascii";
-            break;
-        default:
-            break;
+// // on auto/manual checkbox toggle
+// document.getElementById("auto_manual").addEventListener("change", function(e) {
+//     if (document.getElementById("auto_manual").checked) {
+//         console.log("checked");
+//         document.getElementById("manual_buttons").disabled = true;
+//         auto_number_detection = true;
+//         parse_input(document.getElementById("input").value);
+//     }
+//     else {
+//         document.getElementById("manual_buttons").disabled = false;
+//         console.log("un checked");
+//         auto_number_detection = false;
+//     }
+// })
 
-    }
+// // document.getElementsByName("type_select").addEventListener("input", function(e){
+// //     console.log("Radio btn");
+// //     console.log(e);
+// // })
 
-    if (btn_id != "")
-    {
-        document.getElementById(btn_id).checked = true;
-    }
-}
+// if (document.querySelector('input[name="type_select"]')) {
+//     document.querySelectorAll('input[name="type_select"]').forEach((elem) => {
+//         elem.addEventListener("change", function(event) {
+//         manual_entry = event.target.value;
+//         console.log(manual_entry);
+
+//         parse_input(document.getElementById("input").value);
+//       });
+//     });
+//   }
+
+// // set one of the 'manual number representation' radio buttons to checked
+// function set_radio_btn(number_format) {
+
+//     switch(number_format) {
+//         case "Decimal":
+//             btn_id = "select_dec";
+//             break;
+//         case "Hex":
+//             btn_id = "select_hex";
+//             break;
+//         case "Binary":
+//             btn_id = "select_bin";
+//             break;
+//         case "ASCII":
+//             btn_id = "select_ascii";
+//             break;
+//         default:
+//             break;
+
+//     }
+
+//     if (btn_id != "")
+//     {
+//         document.getElementById(btn_id).checked = true;
+//     }
+// }
